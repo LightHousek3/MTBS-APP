ad7e39f47553f2e7e23afc6cc517b4ff99147087
Phạm Quốc Tự <quoctu.se@gmail.com>
2026-06-23 23:04:25 +0700

    fix(Manage Redeem For Customer): fix fetch movie and showtime

--- lib/features/ticket_prices/data/ticket_price_data_providers.dart
+++ lib/features/ticket_prices/data/ticket_price_data_providers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mtbs_app/core/di/core_providers.dart';
-import 'package:mtbs_app/core/network/dio_client.dart';
 import 'package:mtbs_app/features/ticket_prices/data/repositories/ticket_price_repository_impl.dart';
 import 'package:mtbs_app/features/ticket_prices/data/services/ticket_price_api_service.dart';
 import 'package:mtbs_app/features/ticket_prices/domain/repositories/ticket_price_repository.dart';
